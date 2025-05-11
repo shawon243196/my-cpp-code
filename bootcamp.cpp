d3002f8b81a09aa2daa471658c4b7e0f310e876f
shawon243196 <sadmanshawon542@gmail.com>
2025-05-11 10:17:58 +0600

    Add files via upload

--- bootcamp.cpp
+++ bootcamp.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include<iomanip>
+#include<string.h>
+using namespace std;
+
+
+int main()
+{
+
+  string a;
+  string b;
+  cin>>a>>b;
+  int x=a.size();
+  int y=b.size();
+  cout<<x<<" "<<y<<endl;
+  cout<<a<<" "<<b<<endl;
+return 0;
+}
 
