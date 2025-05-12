7dffb2f481ed892e0964669f1a792d69d79d398d
shawon243196 <sadmanshawon542@gmail.com>
2025-05-12 22:18:27 +0600

    Add files via upload

---
+++ Cloudberry Jam.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+//#include<bits/stdc++.h>
+#include <string>
+//#include <cctype>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+int t;
+cin>>t;
+while(t--)
+{
+    int n;
+    cin>>n;
+    cout<<n*2<<endl;
+}
+    return 0;
+}
