37c63f5b47957a8166c5c7d962635b3bfcee39a8
Mohammad sadman shawon <sadmanshawon542@gmail.com>
2025-11-08 21:13:47 +0600

    Create STL pair.cpp

---
+++ STL pair.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+    vector<pair<int,int>>v;
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++)
+  {
+    int x,y;
+    cin>>x>>y;
+    v.push_back(make_pair(x,y));
+  }
+  for(auto i:v)
+  {
+    cout<<i.first<<" "<<i.second<<endl;
+  } 
+    return 0;
+}
